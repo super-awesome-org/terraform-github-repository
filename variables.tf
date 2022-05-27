ee1a7a08692c90234e3d55f298bf419bd47254bf
Tobias Estefors <102219755+dsz182@users.noreply.github.com>
2022-05-27 15:48:04 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -109,7 +109,7 @@ variable "archive_on_destroy" {
   default     = false
 }
 
-variable "template" {
+variable "pages" {
   type        = map(any)
   description = "Configuration block for GitHub Pages"
   default     = {}
